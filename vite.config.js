7e30f8867682dba2add55e7671d6c82ff70b1911
Ajay choudhary <142705287+ajayahirwar93400@users.noreply.github.com>
2025-05-07 19:30:52 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Kedar-online-games/',
+  base: './',
   plugins: [react()],
 });
